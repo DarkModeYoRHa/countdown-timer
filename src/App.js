db0c6ee82aa7cd7badbe7a47f960651c4e8cf527
Tim Sweet <timothy.sweet@jorsek.com>
2021-01-16 20:43:36 -0800

    wick image and update the math part to actually work...

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import './App.css';
 import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
+const SECOND = 1000; // each second is 1000 miliseconds
+const MINUTE = SECOND * 60; // 60 seconds per minute
+const HOUR = MINUTE * 60; // 60 minutes per hour
+const DAY = HOUR * 24; // 24 hours per day
+
 function App() {
   let [currentTime, setCurrentTime]=useState (new Date())
   setInterval(() => {
     setCurrentTime(new Date())
   },1000)
+
   let targetDay = new Date(Date.parse("03 August 2021 00:00:00 PST"))
   let diffTime = Math.abs (targetDay - currentTime)
-  let diffDays = Math.ceil (diffTime/(1000*60*60*24))
-  let diffHours = Math.abs(Math.ceil ((diffTime/(1000*60*60) -(diffDays*24))))
-  let diffMinutes = Math.abs(Math.ceil ((diffTime/(1000*60) -(diffDays*24*60))))
-  let diffSeconds = Math.abs(Math.ceil ((diffTime/1000) -(diffDays*24*60*60)))
-  console.log(diffTime)
+  
+  let diffDays = Math.floor (diffTime/DAY)
+  let diffHours = Math.abs(Math.floor ((diffTime % DAY) / HOUR ))
+  let diffMinutes = Math.abs(Math.floor ((diffTime % HOUR) / MINUTE))
+  let diffSeconds = Math.abs(Math.floor ((diffTime % MINUTE) / SECOND))
+  // console.log(diffTime)
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor: "transparent"}}>
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" color="inherit" aria-label="menu">
@@ -29,8 +36,8 @@ function App() {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header" style={{backgroundColor: "transparent"}}>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
           i can't wait to see john wick
